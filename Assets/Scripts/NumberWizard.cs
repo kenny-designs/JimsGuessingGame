e71ce0b09a657ff0e6b89120a9c3fb607e2e83b2
Alexander Aguilar <alexanderman2004@yahoo.com>
2019-03-21 23:46:22 -0400

    follow up commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+  // highest or lowest 
+  [SerializeField] int max, min;
+  [SerializeField] TextMeshProUGUI guessText = null;
+
+  int guess;
+
+  // Start is called before the first frame update
+  void Start() {
+    StartGame();
+  }
+
+  // our own initialization function
+  void StartGame() {
+    NextGuess();
+  }
+
+  public void OnPressHigher() {
+    if (min >= max) return;
+
+    min = guess + 1;
+    NextGuess();
+  }
+
+  public void OnPressLower() {
+    if (min >= max) return;
+
+    max = guess - 1;
+    NextGuess();
+  }
+
+  void NextGuess() {
+    guess = Random.Range(min, max + 1);
+    guessText.text = (guess.ToString());
+  }
+}
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+  [SerializeField] float spinSpeed = 1;
+  [SerializeField] float maxSpin = 30f;
+
+
+  void Update() {
+    transform.rotation = Quaternion.Euler(0f, maxSpin * Mathf.Sin(Time.time * spinSpeed), 0f);
+  }
+}
